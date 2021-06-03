6de841469ae072c06359e547c1912e3cb407767b
xClaudiaArias <xclaudiaarias@gmail.com>
2021-06-02 23:16:13 -0400

    added testimonials

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-let slides = document.querySelectorAll(".testimonies")
+// let slides = document.querySelectorAll(".testimonies")
 
-function sliderTimer () {
-    let slideIndex = 1;
-    let currentSlide = slideIndex - 1;
+// function sliderTimer () {
+//     let slideIndex = 1;
+//     let currentSlide = slideIndex - 1;
 
-    slides.forEach(slide => {
-        slide.style.display = 'none';
-    })
+//     slides.forEach(slide => {
+//         slide.style.display = 'none';
+//     })
 
-    setTimeout(function(){
-        if (slideIndex > slides.length){
-            slideIndex = 1;
-        }
-        if (slideIndex < 1){
-            slideIndex = slides.length
-        }
-        slideIndex += 1;
-        slides[slideIndex].style.display = "block";
-        console.log(slideIndex, 'slideIndex')
-    }, 3000)
-}
+//     setTimeout(function(){
+//         if (slideIndex > slides.length){
+//             slideIndex = 1;
+//         }
+//         if (slideIndex < 1){
+//             slideIndex = slides.length
+//         }
+//         slideIndex += 1;
+//         slides[slideIndex].style.display = "block";
+//         console.log(slideIndex, 'slideIndex')
+//     }, 3000)
+// }
 
-sliderTimer()
+// sliderTimer()
